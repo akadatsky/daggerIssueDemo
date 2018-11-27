1050adba0ae6e8dd975ced555243ccbbc44eacda
Andrey Kadatsky <andrey.kadatsky@gmail.com>
2018-11-27 11:46:13 +0200

    add import

--- app/src/main/java/com/akadatsky/testdaggerissue/App.kt
+++ app/src/main/java/com/akadatsky/testdaggerissue/App.kt
@@ -1,5 +1,7 @@
 package com.akadatsky.testdaggerissue
 
+
+import com.akadatsky.testdaggerissue.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
